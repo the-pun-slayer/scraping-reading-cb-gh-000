2ae425f3378a220c75f329a3b0367d018b2dc14e
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-09 11:02:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN")
 doc.css(".headline-26OIBN").text
+puts doc.css(".headline-26OIBN")
