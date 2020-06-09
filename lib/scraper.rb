5f4f83392481441608412268b9eaf87cd9e56b40
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-09 11:02:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+doc.css(".headline-26OIBN").text
+puts doc.css(".headline-26OIBN")
